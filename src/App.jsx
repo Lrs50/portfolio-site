ce79abd277f3c5c803d076f8ff4192fbd6bbff6e
lrs50 <lucaspook12@gmail.com>
2025-04-13 11:28:17 -0300

    ADD: Website file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Card from './components/CardItem'
+import Card from './components/Cardproject'
 
 function Botao(){
     return <button className="bg-blue-500 text-white px-4 py-2 rounded"> Click me </button>
